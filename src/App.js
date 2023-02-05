5fa266600af7a73ba7ab715b8d5abdd271a597fe
harshvardhan2905 <harsh80183@gmail.com>
2023-02-06 00:47:55 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Content from './components/Content';
 import Example from './components/Chart';
+import ToolBar from './components/ToolBar';
+import Details from './components/Details';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       < Navbar />
       <Content />
       <Example />
+      <ToolBar/>
+      <Details star = "1" view = "1292" published ="Aug28,2020" Updated = "Dec2,2022" />
+      <Footer/>
     </div >
   );
 }
